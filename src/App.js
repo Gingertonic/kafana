2fbf7a745cd504d0f1d6b67f6d64748879cd6836
Gingertonic <bethmschofield@gmail.com>
2020-05-28 16:13:10 +0100

    basic test routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Chat } from './components'
 
 function App() {
   return (
     <div className="App">
       <h1>Kafana</h1>
+      <Link to="/chat">Chat</Link>
+      <Switch>
+        <Route path="/chat" component={Chat} />
+      </Switch>
     </div>
   );
 }
